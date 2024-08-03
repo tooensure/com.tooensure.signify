a7a3281a66af21a3f6dffc3bae5cf39d1afe9e16
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-08-03 02:13:52 -0500

    Updatre

--- Signify.Api/Program.cs
+++ Signify.Api/Program.cs
@@ -1,6 +1,36 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Register the Swagger generator, defining 1 or more Swagger documents
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Signify API",
+        Description = "Simplify mobile app signing with a containerized .Net API that generates PFX files and manages data in Firestore.",
+        TermsOfService = new Uri("https://agreeable-island-056108010.5.azurestaticapps.net"),
+        Contact = new OpenApiContact
+        {
+            Name = "Github",
+            Email = string.Empty,
+            Url = new Uri("https://github.com/Shawn-Bellazan-jr"),
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Use under MIT",
+            Url = new Uri("https://github.com/Shawn-Bellazan-jr/Signify?tab=MIT-1-ov-file#readme"),
+        }
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -11,7 +41,17 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // Enable middleware to serve generated Swagger as a JSON endpoint.
+    app.UseSwagger();
+
+    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+    // specifying the Swagger JSON endpoint.
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // To serve the Swagger UI at the app's root
+    });
+    
 }
 
 app.UseHttpsRedirection();
